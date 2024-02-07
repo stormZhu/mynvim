a537aba2af2d0d3a6d9fdc7204ab93bedc18df50
stormZhu <779724979@qq.com>
2024-02-07 14:59:32 +0800

    初始化配置

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,16 @@
+return {
+  "akinsho/bufferline.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  version = "*",
+  opts = {
+    options = {
+      -- mode = "tabs",  -- buffers和nvim-tree好像有冲突，会报一下错，但是不影响使用
+      -- separator_style = "slant",
+      -- hover = {
+      --   enabled = true,
+      --   delay = 200,
+      --   reveal = {'close'}
+      -- }
+    },
+  },
+}
